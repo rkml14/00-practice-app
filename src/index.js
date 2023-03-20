3c53463bfb8372cd1b7b6cb7538515dcc1ea6a1e
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-03-20 14:28:10 -0400

    trying again

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// TODO: Add a comment giving a brief description of what React is
 import React from 'react';
+
+// TODO: Add a comment that describes the difference between react and react-dom
 import ReactDOM from 'react-dom';
+
 import App from './App';
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
 
-
+// TODO: Add a comment describing the significance of the ReactDOM.render method
 ReactDOM.render(<App />, document.getElementById('root'));
